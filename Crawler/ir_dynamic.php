ad98676372045f73e922fadeef58f8b7018ca377
aryaniparamita <aryaniparamita@gmail.com>
2016-03-27 15:06:12 +0800

    Update ir_dynamic.php

--- Crawler/ir_dynamic.php
+++ Crawler/ir_dynamic.php
@@ -80,7 +80,7 @@ fclose($time);
 	// use key 'http' even if you send the request to https://...
 	$options = array(
 		'http' => array(
-			'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+		//	'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
 			'method'  => 'POST',
 			'content' => http_build_query($data)
 		)
